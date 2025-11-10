d0e6ff7105ea77e858b2da639b125df10fb65546
Flexicos <anikwenzefelix@gmail.com>
2025-11-10 20:07:16 +0100

    Add files via upload

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+if (Args.length ===0){
+    console.log("No argument");
+} else if(args.length ===1){
+    console.log("Argument found");
+} else{
+    console.log("Arguments found");
+}
+console.log(...)
