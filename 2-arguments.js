b599c0db7df676a130c57030b64ece8aa321f310
anikwenzefelix@gmail.com <anikwenzefelix@gmail.com>
2025-11-10 20:30:22 +0100

    feat: add 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,9 @@
-if (Args.length ===0){
+const args= process.argv.slice(2);
+if (args.length ===0){
     console.log("No argument");
-} else if(args.length ===1){
+} else if (args.length ===1){
     console.log("Argument found");
-} else{
+}else{
     console.log("Arguments found");
 }
 console.log(...)
