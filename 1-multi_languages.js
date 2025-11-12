dc0f0113a298096c46af8558f2b01b02f4b2b6af
anikwenzefelix@gmail.com <anikwenzefelix@gmail.com>
2025-11-12 04:04:49 +0100

    feat: add 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,6 +1,7 @@
-const firstline="C is fun";
-const Secondline="Python is cool";
-const thirdline="Javascript is amazing";
-console.log(firstline);
-console.log(Secondline);
-console.log(thirdline);
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
