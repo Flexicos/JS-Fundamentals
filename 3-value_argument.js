a7c2a0c38aa5be65ffd8b344a1fe400366568855
anikwenzefelix@gmail.com <anikwenzefelix@gmail.com>
2025-11-12 03:49:04 +0100

    feat: add 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,8 +1,7 @@
-const args = process.argv.slice(2); 
+const arg = process.argv[2];
 
-if (args[0] === undefined) {
-    console.log("No argument");      
+if (arg === undefined) {
+  console.log('No argument');
 } else {
-    console.log(args[0]);            
+  console.log(arg);
 }
-console.log(args[0] === undefined ? "No argument" : args[0]);
